0c7a6a9f9c2024aea01c7ef15b7d553d01f1690a
Noah Trier <noah30t@gmail.com>
2025-01-01 17:47:21 +0100

    PageHeader Design V1

--- webpage/src/lib/components/App/Header.svelte
+++ webpage/src/lib/components/App/Header.svelte
@@ -19,17 +19,38 @@
     }
 </script>
 
-<div class="header mt-3 mx-3 rounded-tl-2xl rounded-tr-2xl dark:bg-surface-600 bg-tertiary-200 py-5 px-3">
-    <div class="container">
-        <!-- Hamburger button to trigger sidebar opening on mobile devices -->
-        <button 
-            class="hamburger w-8 h-8 flex items-center justify-center 2xl:hidden" 
-            on:click={openSidebar} 
-            on:keydown={(e) => e.key === 'Enter' && openSidebar()} 
-            aria-label="Open sidebar"
-        >
-            <i class="fa-solid fa-bars p-3 bg-tertiary-400 dark:bg-[#1a1a1a] rounded-lg cursor-pointer"></i>
-        </button>
-        <p>pageHeader</p>
+<div class="header mt-3 mx-3 rounded-tl-2xl rounded-tr-2xl dark:bg-surface-600 bg-tertiary-200 px-6">
+    <div class="container flex justify-between items-center">
+        <div>
+            <!-- Hamburger button to trigger sidebar opening on mobile devices -->
+            <button 
+                class="hamburger w-8 h-8 flex items-center justify-center 2xl:hidden" 
+                on:click={openSidebar} 
+                on:keydown={(e) => e.key === 'Enter' && openSidebar()} 
+                aria-label="Open sidebar"
+            >
+                <i class="fa-solid fa-bars p-3 bg-tertiary-400 dark:bg-[#1a1a1a] rounded-lg cursor-pointer"></i>
+            </button>
+            <div class="space-x-5">
+                <button aria-label="Notifications" class="relative">
+                    <span class="badge-icon variant-filled-warning absolute -top-2 -right-2 z-10">2</span>
+                    <i class="fa-regular fa-bell dark:bg-[#1d1d1d] bg-tertiary-400 py-3 px-4 rounded-lg"></i>
+                </button>
+                <button aria-label="Help" class="relative">
+                    <i class="fa-regular fa-circle-question dark:bg-[#1d1d1d] bg-tertiary-400 py-3 px-4 rounded-lg"></i>
+                </button>
+            </div>
+        </div>
+        <div>
+            <img src="/images/logos/SpinLuckZone.png" alt="SpinLuckZone_Logo" class="w-20 h-20" draggable="false" />
+        </div>
+        <div class="flex items-center">
+            <div class="dark:bg-[#1d1d1d] w-36 h-6 rounded-full flex justify-center items-center">
+                <p class="text-xs select-none">Lvl (cooming soon)</p>
+            </div>
+            <button class="bg-tertiary-400 dark:bg-[#1d1d1d] p-1 rounded-2xl flex justify-center items-center ml-10">
+                <img src="/images/icons/user.png" alt="User_Logo" class="w-10 h-10 rounded-full" draggable="false">
+            </button>
+        </div>
     </div>
 </div>
